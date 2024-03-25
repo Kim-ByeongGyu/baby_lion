df76398f7edbe46d49c3e5e801158c135c12cf9a
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-03-25 19:55:57 +0900

    if else

---
+++ javaExam/src/main/java/day02/AgeChecker.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.util.Scanner;
+
+public class AgeChecker {
+    public static void checker (int age) {
+        if (age < 18) {
+            System.out.println("미성년자입니다.");
+        } else {
+            System.out.println("성인입니다.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("나이를 입력하세요");
+        System.out.print("입력 : ");
+        int age = sc.nextInt();
+        checker(age);
+    }
+}
